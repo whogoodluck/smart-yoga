093f043f9f7a4492238ae7ccf171984fd955605a
SunnyKumar107 <goodluck14115@gmail.com>
2024-11-18 20:57:47 +0530

    fix: deploy error

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -2,5 +2,6 @@ import { Poppins } from 'next/font/google'
 
 export const poppins = Poppins({
   weight: ['400', '500', '600', '700', '800', '900'],
-  variable: '--font-poppins'
+  variable: '--font-poppins',
+  subsets: ['latin']
 })
